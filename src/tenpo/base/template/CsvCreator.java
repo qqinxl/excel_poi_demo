38dfec357a743a749a3bf34f973d9e89993b8b4b
qqinxl <qqinxl@gmail.com>
2012-10-01 09:43:57 +0900

    add x x x

--- src/tenpo/base/template/CsvCreator.java
+++ src/tenpo/base/template/CsvCreator.java
@@ -9,66 +9,90 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class CsvCreator {
 
-	private final static String COMMA = ",";
+    private final static String COMMA = ",";
 
-	public static void setCSVData(final String fileName, final List<LinkedHashMap<String, String>> data) throws IOException {
-		if (fileName == null || data == null || data.isEmpty()) {
-			throw new InvalidParameterException();
-		}
+    public static void setCSVData(final String fileName,
+            final List<LinkedHashMap<String, String>> data) throws IOException {
+        if (fileName == null || data == null || data.isEmpty()) {
+            throw new InvalidParameterException();
+        }
 
-		File csv = new File(fileName);
-		BufferedWriter bw = new BufferedWriter(new FileWriter(csv));
+        List<String> header = new ArrayList<String>(data.get(0).keySet());
+        List<List<String>> listData = new ArrayList<List<String>>();
 
-		Set<String> keySet = data.get(0).keySet();
-		int length = keySet.size();
+        for (LinkedHashMap<String, String> lineData : data) {
+            List<String> tmpData = new ArrayList<String>();
+            for (Map.Entry<String, String> e : lineData.entrySet()) {
+                String value = e.getValue();
+                tmpData.add(value);
+            }
+            listData.add(tmpData);
+        }
 
-		int num = 1;
-		for (String key : keySet) {
-			bw.write(key);
-			if (num < length) {
-				bw.write(COMMA);
-			}
-			num++;
-		}
+        setCSVData(fileName, header, listData);
+    }
 
-		for (LinkedHashMap<String, String> lineData : data) {
-			bw.newLine();
-			num = 1;
-			for (Map.Entry<String, String> e : lineData.entrySet()) {
-				String value = e.getValue();
-				bw.write(value == null ? "": value);
-				if (num < length) {
-					bw.write(COMMA);
-				}
-				num++;
-			}
-		}
+    public static void setCSVData(final String fileName,
+            final List<String> header,
+            final List<List<String>> data) throws IOException {
 
-		bw.close();
+        if (fileName == null || header == null || header.isEmpty() || data == null || data.isEmpty()) {
+            throw new InvalidParameterException();
+        }
 
-	}
+        File csv = new File(fileName);
+        BufferedWriter bw = new BufferedWriter(new FileWriter(csv));
 
-	public static void main(final String[] args) throws IOException {
-		List<LinkedHashMap<String, String>> data = new ArrayList<LinkedHashMap<String, String>>();
+        int length = header.size();
 
-		LinkedHashMap<String, String> line1 = new LinkedHashMap<String, String>();
-		line1.put("A11111", "11111");
-		line1.put("Z22222", null);
-		line1.put("B33333", "33333");
-		line1.put("Y44444", "44444");
-		data.add(line1);
+        int num = 1;
+        for (String key : header) {
+            bw.write(key);
+            if (num < length) {
+                bw.write(COMMA);
+            }
+            num++;
+        }
 
-		LinkedHashMap<String, String> line2 = new LinkedHashMap<String, String>();
-		line2.put("A11111", "aaaaa");
-		line2.put("Z22222", "zzzzz");
-		line2.put("B33333", "bbbbb");
-		line2.put("Y44444", "yyyyy");
-		data.add(line2);
+        for (List<String> lineData : data) {
+            bw.newLine();
+            num = 1;
+            for (String value : lineData) {
+                bw.write(value == null ? "" : value);
+                if (num < length) {
+                    bw.write(COMMA);
+                }
+                num++;
+            }
+        }
 
-		setCSVData("test.csv", data);
-	}
+        bw.close();
+
+    }
+
+    public static void main(final String[] args) throws IOException {
+        List<LinkedHashMap<String, String>> data =
+                new ArrayList<LinkedHashMap<String, String>>();
+
+        LinkedHashMap<String, String> line1 =
+                new LinkedHashMap<String, String>();
+        line1.put("A11111", "11111");
+        line1.put("Z22222", null);
+        line1.put("B33333", "33333");
+        line1.put("Y44444", "44444");
+        data.add(line1);
+
+        LinkedHashMap<String, String> line2 =
+                new LinkedHashMap<String, String>();
+        line2.put("A11111", "aaaaa");
+        line2.put("Z22222", "zzzzz");
+        line2.put("B33333", "bbbbb");
+        line2.put("Y44444", "yyyyy");
+        data.add(line2);
+
+        CsvCreator.setCSVData("test.csv", data);
+    }
 }
